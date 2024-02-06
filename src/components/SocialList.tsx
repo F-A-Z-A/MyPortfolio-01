83688b80e2cbef5ff2a3c138a4fd7293acf4ac26
AndreyUrazaev <f.a.z.a@bk.ru>
2024-02-06 16:45:29 +0500

    portfolio styling

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -3,36 +3,36 @@ import {Icon} from "./icons/Icon";
 import styled from "styled-components";
 
 export const SocialList = () => {
-   return (
-      <StyledSocialList>
-         <SocialItem>
-            <SocialLink>
-               <Icon height={"35px"} width={"35px"} viewBox={"0 0 35px 35px"} iconId={"myGithub"}/>
-            </SocialLink>
-         </SocialItem>
-         <SocialItem>
-            <SocialLink>
-               <Icon height={"35px"} width={"35px"} viewBox={"0 0 35px 35px"} iconId={"myLinkedin"}/>
-            </SocialLink>
-         </SocialItem>
-         <SocialItem>
-            <SocialLink>
-               <Icon height={"35px"} width={"40px"} viewBox={"0 0 35px 40px"} iconId={"myTelegram"}/>
-            </SocialLink>
-         </SocialItem>
-      </StyledSocialList>
-   );
+  return (
+    <StyledSocialList>
+      <SocialItem>
+        <SocialLink>
+          <Icon height={"35px"} width={"35px"} viewBox={"0 0 35px 35px"} iconId={"myGithub"}/>
+        </SocialLink>
+      </SocialItem>
+      <SocialItem>
+        <SocialLink>
+          <Icon height={"35px"} width={"35px"} viewBox={"0 0 35px 35px"} iconId={"myLinkedin"}/>
+        </SocialLink>
+      </SocialItem>
+      <SocialItem>
+        <SocialLink>
+          <Icon height={"35px"} width={"40px"} viewBox={"0 0 35px 40px"} iconId={"myTelegram"}/>
+        </SocialLink>
+      </SocialItem>
+    </StyledSocialList>
+  );
 };
 
 const StyledSocialList = styled.ul`
-    display: flex;
-    gap: 30px;
+  display: flex;
+  gap: 30px;
 `
 
 const SocialItem = styled.li``
 
 const SocialLink = styled.a`
-    &:hover {
-        color: #13ADC7;
-    }
+  &:hover {
+    color: #13ADC7;
+  }
 `
